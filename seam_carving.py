5f9967c3676934dbb9b6d9f3d0a79221cfbd20a7
Erwin2409 <83054910+erwin24092002@users.noreply.github.com>
2022-05-31 20:51:41 +0700

    upload

--- seam_carving.py
+++ seam_carving.py
@@ -12,5 +12,17 @@ class SeamCarving:
         self.img = img 
         self.new_img = img.copy() 
         self.sliders = []  
-        
     
+    @jit
+    def gen_emap(self):
+        """Generate an energy map using Gradient magnitude
+        Args:
+        Returns:
+            np.array: an energy map (emap) of input image 
+        """
+        Gx = ndi.convolve1d(self.new_img, np.array([1, 0, -1]), axis=1, mode='wrap')
+        Gy = ndi.convolve1d(self.new_img, np.array([1, 0, -1]), axis=0, mode='wrap')
+        emap = np.sqrt(np.sum(Gx**2, axis=2) + np.sum(Gy**2, axis=2))
+        return emap
+
+
