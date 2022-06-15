bb74415fa6a188dab8267f82cc59b887e7c61a4f
Trương Thành Thắng <83054910+erwin24092002@users.noreply.github.com>
2022-06-15 20:15:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,33 @@ import cv2
 import numpy as np
 from seam_carving import SeamCarving
 
-img = cv2.imread('img/img2.png', 1)
+# DEMO:
+# Read image
+img = cv2.imread('img/khinhkhicau.jpeg', 1)
 
+# Init Seam Carving Object
 sc = SeamCarving(img)
-mask = sc.get_mask()
-cv2.imwrite('mask/test_mask.jpg', mask)
 
-emap = sc.gen_emap()
-cv2.imwrite('result/emap2.jpg', emap.astype(np.uint8))
+# Get mask
+re_mask = sc.get_mask(desc='Locating the Removal Object') # removal mask
+cv2.imwrite('mask/re_khinhkhicau.jpeg', re_mask)
+pro_mask = sc.get_mask(desc='Locating the Protective Object') # protective mask
+cv2.imwrite('mask/pro_khinhkhicau.jpeg', pro_mask)
 
-smap = sc.gen_smap(emap)
-cv2.imwrite('result/smap2.jpg', smap.astype(np.uint8))
 
-result = sc.remove_object(mask)
-cv2.imwrite('result/result2.jpg', result)
+# Remove Object
+# result = sc.remove_object(removal_mask=re_mask)
+result = sc.remove_object(removal_mask=re_mask, protective_mask=pro_mask)
+cv2.imwrite('result/khinhkhicau.jpeg', result)
 
-sc.visual_process('visual_process/process.gif')
+# Visualizing process
+sc.visual_process('visual_process/khinhkhicau.gif')
+
+
+# emap = sc.gen_emap()
+# smap = sc.calc_backward_emap(emap)
+
+# cv2.imwrite('emap.jpg', emap)
+# cv2.imwrite('bemap.jpg', smap.astype(np.uint8))
+
 
